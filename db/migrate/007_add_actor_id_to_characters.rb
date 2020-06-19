240310ad434f991517d226a640b00aad2a221ba2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 20:50:11 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_actor_id_to_characters.rb
+++ db/migrate/007_add_actor_id_to_characters.rb
@@ -1,7 +1,7 @@
 class AddActorIdToCharacters < ActiveRecord::Migration[5.2]
 
 def change
-   add_column :characters, :actor_id, :integer
+   add_column :characters, :actor_id, :integer, :actors_list
  end
 
 end
