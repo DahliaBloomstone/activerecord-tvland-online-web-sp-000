95d215a870c5639566488e0a897b235014185825
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 20:39:40 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,3 +3,6 @@ class Show < ActiveRecord::Base
   has_many :characters, through: :characters
   has_many :characters
 end
+
+
+#This doesn't save these instances, but will set up the right associations for us and when we save our character, the new show and network are also saved.
